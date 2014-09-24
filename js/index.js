c5c73a21b12a86e541f757bd789f0f0957378f6f
Facu Guini <facundoguini20@hotmail.com>
2014-09-23 23:39:31 -0300

    varios2

--- js/index.js
+++ js/index.js
@@ -17,13 +17,22 @@ $( document ).ready(function() {
 		tabs("jug");	
 	});
 	$("#rtabla").click(function(){
-		$(this).toggleClass("test")
-		$('#tablaweb').load(document.URL + ' #tablaweb');
-		$('#tablaweb').effect("highlight");
+		animate($(this));
+		iframe.load(function(){
+			iframe.contents().find(".bottom-line, #tab-tabla-1692, #tab-fairplay, #tab-vallamenosvencida, #tabla-posiciones-1692 .col3, .col4, .col5, .col6, .col7, .col9, .col-fp, .instructions").hide();
+			iframe.contents().find('.titles').css("background-color", "#000069");
+			iframe.contents().find('.titles').css("width", "100%");
+			iframe.contents().find('.col1').css("width", "40%");		
+			iframe.contents().find('.col2, .col8, .col10').css("width", "20%");
+		})
+		$('#tablaweb').effect("fade");
+		setTimeout(function() {$( "#tablaweb" ).fadeIn();}, 350 );
 	});
 	$("#rfecha").click(function(){
+		animate($(this));
 		$('#fechaweb').load(document.URL + ' #fechaweb');
-		$('#fechaweb').effect("highlight");
+		$('#fechaweb').effect("fade");
+		setTimeout(function() {$( "#fechaweb" ).fadeIn();}, 350 );
 	});
 
 	var iframe = $('#itable');
@@ -33,6 +42,10 @@ $( document ).ready(function() {
 		iframe.contents().find('.titles').css("width", "100%");
 		iframe.contents().find('.col1').css("width", "40%");		
 		iframe.contents().find('.col2, .col8, .col10').css("width", "20%");
+	})
+
+	$("#iwfecha").click(function() {
+		$('#imgModal').modal('toggle');
 	})
 });
 
@@ -57,3 +70,11 @@ function tabs(id) {
 			break;
 	}
 }
+
+var animate = function(animar) {
+	    animateClass = "glyphicon-refresh-animate";
+	    animar.addClass( animateClass );
+  	     window.setTimeout( function() {
+     	        animar.removeClass( animateClass );
+             }, 1000 );  
+	}
