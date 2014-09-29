37ea182880c8fa0d4f844a2042f9fcd61c2b4915
Facu Guini <facundoguini20@hotmail.com>
2014-09-29 16:18:36 -0300

    varios4

--- js/index.js
+++ js/index.js
@@ -31,9 +31,16 @@ $( document ).ready(function() {
 	});
 	$("#rfecha").click(function(){
 		animate($(this), "rotate");
-		$('#iwfecha').attr("src","http://www.360sports.com.ar/images/horarios_domingos.jpg");
+		//DownloadFile("http://360sports.com.ar/images/horarios_domingos.jpg", "images", "horarios_domingos");
+		$('#iwfecha').attr("src","images/horarios_domingos.jpg");
 		$('#fechaweb').effect("fade");
 		setTimeout(function() {$( "#fechaweb" ).fadeIn();}, 350 );
+	});
+	$("#rjug").click(function(){
+		animate($(this), "rotate");
+		$('#ljug').effect("fade");
+		$('#ljug').load();
+		setTimeout(function() {$( "#ljug" ).fadeIn();}, 350 );
 	});
 
 	var iframe = $('#itable');
@@ -77,6 +84,9 @@ $( document ).ready(function() {
 	$("#inv").on( 'click', '.glyphicon-remove', function () {
 		$(this).closest("div").remove();
 	})
+
+
+	DownloadFile("http://360sports.com.ar/images/horarios_domingos.jpg", "images", "horarios_domingos");
 });
 
 
@@ -108,4 +118,64 @@ var animate = function(animar, animation) {
 function isNullOrWhiteSpace( input ) {
     if (input == null) return true;
     return input.replace(/\s/g, '').length < 1;
+}
+
+//First step check parameters mismatch and checking network connection if available call    download function
+function DownloadFile(URL, Folder_Name, File_Name) {
+	if (URL == null && Folder_Name == null && File_Name == null) {
+	    return;
+	}
+	else {
+	   	var networkState = navigator.connection.type;
+	    if (networkState == Connection.NONE) {
+	        return;
+	    } else {
+	        download(URL, Folder_Name, File_Name); //If available download function call
+	    }
+	}
+}
+
+function download(URL, Folder_Name, File_Name) {
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
+
+function fileSystemSuccess(fileSystem) {
+    var download_link = encodeURI(URL);
+    ext = download_link.substr(download_link.lastIndexOf('.') + 1); //Get extension of URL
+
+    var directoryEntry = fileSystem.root; // to get root path of directory
+    directoryEntry.getDirectory(Folder_Name, { create: true, exclusive: false }, onDirectorySuccess, onDirectoryFail); // creating folder in sdcard
+    var rootdir = fileSystem.root;
+    var fp = rootdir.fullPath; // Returns Fulpath of local directory
+
+    fp = fp + "/" + Folder_Name + "/" + File_Name + "." + ext; // fullpath and name of the file which we want to give
+    // download function call
+    filetransfer(download_link, fp);
+}
+
+function onDirectorySuccess(parent) {
+}
+
+function onDirectoryFail(error) {
+    alert("Unable to create new directory: " + error.code);
+}
+
+  function fileSystemFail(evt) {
+    alert(evt.target.error.code);
+ }
+}
+
+function filetransfer(download_link, fp) {
+var fileTransfer = new FileTransfer();
+// File download function with URL and local path
+fileTransfer.download(download_link, fp,
+                    function (entry) {
+                        alert("download complete: " + entry.fullPath);
+                    },
+                 function (error) {
+                     //Download abort errors or download failed errors
+                     alert("download error source " + error.source);
+                     //alert("download error target " + error.target);
+                     //alert("upload error code" + error.code);
+                 }
+            );
 }
